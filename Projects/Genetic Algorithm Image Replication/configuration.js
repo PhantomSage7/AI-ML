e13b7baabc1915a127b83a74285c18d6afb21b3b
Shreyanshu Yadav <117184680+PhantomSage7@users.noreply.github.com>
2023-11-06 23:19:06 +0530

    Add files via upload

---
+++ Projects/Genetic Algorithm Image Replication/configuration.js
@@ -0,0 +1,18 @@
+
+const Modes = {
+  TRIANGLE: 'triangle',
+  RECTANGLE: 'rectanlge',
+  CIRCLE: 'circle',
+  CHAR: 'char'
+}
+let CONFIG = {
+  MIN_ITEMS: 1000,
+  MAX_ITEMS: 1500,
+  MAX_DIST_ITEM: 3,
+  POP: 10,
+  MUTATE_RATE: 10,
+  FITNESS: 'RMS',
+  BACKCOLOR: "rgba(0, 0, 0, 1)",
+  MODE: Modes.TRIANGLE,
+  IMAGE: Images.MonaLisa
+};
